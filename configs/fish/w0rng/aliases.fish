23231c292eb5b35f4c1d48d2f401a092d39215e4
w0rng <service+github@w0rng.ru>
2024-07-05 11:26:01 +0700

    add cookiecutter and lsd

--- configs/fish/w0rng/aliases.fish
+++ configs/fish/w0rng/aliases.fish
@@ -8,6 +8,7 @@ alias drm='docker rm -f $(docker ps -aq)'
 
 alias rm="rip"
 alias cat="bat"
+alias ls="lsd"
 
 alias m='set dir (pwd); while test ! -e "$dir/Makefile" -a "$dir" != "/"; set dir (dirname "$dir"); end; test -e "$dir/Makefile"; and make -C "$dir" $argv'
 alias cb='pbcopy'
