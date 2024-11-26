969958f7afe596bcd5cdcd11ac2c3e85fa08df5b
w0rng <service+github@w0rng.ru>
2024-11-26 12:25:45 +0700

    какие-то изменения

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -7,8 +7,3 @@ test -r ~/.fish.env; and export (cat ~/.fish.env|xargs -L 1)
 for f in (find ~/.config/fish/w0rng/ -type f  -name '*.fish')
     source $f
 end
-
-
-fish_add_path /Users/w0rng/Library/Application\ Support/JetBrains/Toolbox/scripts
-fish_add_path /opt/homebrew/sbin
-fish_add_path /Users/w0rng/yandex-cloud/bin
---
+++ configs/fish/w0rng/path.fish
@@ -0,0 +1,4 @@
+fish_add_path /Users/w0rng/Library/Application\ Support/JetBrains/Toolbox/scripts
+fish_add_path /opt/homebrew/sbin
+fish_add_path /Users/w0rng/yandex-cloud/bin
+fish_add_path (go env GOPATH)/bin
