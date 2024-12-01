2cc5e299618aa66b0a3ed4e4eb0e0a3d046cf7c3
w0rng <service+github@w0rng.ru>
2024-12-01 11:25:16 +0700

    fix path fish

--- configs/fish/w0rng/path.fish
+++ configs/fish/w0rng/path.fish
@@ -1,4 +1,4 @@
-fish_add_path /Users/w0rng/Library/Application\ Support/JetBrains/Toolbox/scripts
+fish_add_path "$HOME/Users/w0rng/Library/Application Support/JetBrains/Toolbox/scripts"
 fish_add_path /opt/homebrew/sbin
-fish_add_path /Users/w0rng/yandex-cloud/bin
+fish_add_path "$HOME/yandex-cloud/bin"
 fish_add_path (go env GOPATH)/bin
